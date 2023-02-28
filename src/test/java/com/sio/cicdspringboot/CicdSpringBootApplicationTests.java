ec5be730277477d8c9a6f9138a9ac15b87409a4d
ove <isiful.ovi@gmail.com>
2023-02-28 15:58:14 +0600

    Add

---
+++ src/test/java/com/sio/cicdspringboot/CicdSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sio.cicdspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
